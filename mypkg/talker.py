e7ff31634bfd0f849bc4614d71d215bf6cffbd1a
yukihanada <hanada@hoge.com>
2022-12-26 22:30:27 +0900

    update

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person, "Person", 10)
+pub = node.create_publisher(Int16, "countup", 10)
 n = 0
 
 def cb():
     global n
-    msg = Person()
-    msg.name = "上田"
-    msg.age = n
+    msg = Int16()
+    msg.data = n
     pub.publish(msg)
     n += 1
 
 node.create_timer(0.5, cb)
 rclpy.spin(node)
+
